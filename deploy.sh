84edc550ed72871c7f4246dbe674845f006466e9
Wojciech Włodarczyk <wojciechwlodarczyk@bluebrick.pl>
2021-06-22 01:00:25 +0200

    typo

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:wojtek9609/oneline-shop.git master:gh-pages
+git push -f git@github.com:wojtek9609/online-shop.git master:gh-pages
 
 cd -
